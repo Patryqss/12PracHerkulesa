8f3b5d9a26b92a90d18e9e77ddabb781b064efb2
Patryqss <gpatryk14@gmail.com>
2020-02-01 22:57:52 +0100

    New news added

--- frontend/src/views/Homepage/index.jsx
+++ frontend/src/views/Homepage/index.jsx
@@ -43,15 +43,20 @@ const Home = () => {
       <div className="newsPage">
         <div className='news'>
         <News
+            header="Beta-Testy rozpoczęte" 
+            date="1 lutego 2020" 
+            text='Dziś rozpoczęły się oficjalne beta-testy "12 Prac Herkulesa". Przez najbliższy miesiąc, razem z 16 testerami będziemy szukać błędów, usprawniać mechanikę oraz doszlifowywać każdy poziom, aby już w marcu wydać w pełni funkcjonalną grę. Jeśli dopiero teraz odkryłeś naszą stronę, wciąż jeszcze nie jest za późno, by dołączyć do grona testerów. Szczegóły jak to zrobić, przeczytasz w poprzednim wpisie.'
+        />
+        <News
             header="Testerzy poszukiwani!" 
             date="25 stycznia 2020" 
             text="Już pierwszego lutego startuje zamknięta beta gry! Jeśli chcesz zostać testerem i jako pierwszy wcielić się w mitycznego Herkulesa, wystarczy, że napiszesz do mnie wiadomość. Możesz to zrobić na stronie przez zakładkę 'Kontakt', albo w prywatnej wiadomości na Facebookowym fanpage'u. Jako tester, zostaniesz dodany do prywatnej grupy, na której będziesz mógł dowiedzieć się o kulisach powstawania gry, będziesz mógł zasugerować, co warto dodać lub zmienić w rozgrywce, a także pojawisz się w zakładce 'testerzy' w napisach w ostatecznej wersji gry. Nie czekaj, zapisz się już dziś!"
-          />
-          <News
+        />
+        <News
             header="Strona ruszyła!" 
             date="25 stycznia 2020" 
             text='Witaj na stronie! Jeśli chcesz dowiedzieć się czegoś więcej o "12 Pracach Herkulesa", wejdź w zakładkę "O grze". Jeśli chcesz się ze mną skontaktować, możesz to zrobić poprzez mój fanpage na Facebooku widoczny po prawej, lub poprzez wejście w zakładkę "Kontakt".'
-          />
+        />
         </div>
         <div className="facebok">
           <div className="fb-page" data-href="https://www.facebook.com/12-Prac-Herkulesa-113511530195213/" data-tabs="timeline" data-width="" data-height="" data-small-header="false" data-adapt-container-width="true" data-hide-cover="false" data-show-facepile="true">
